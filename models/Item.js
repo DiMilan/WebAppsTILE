4a725d0da8d7e85a7f122ed59818c997e20a60a8
milan <milandima@gmail.com>
2018-08-14 14:29:51 +0200

    item.js

--- models/Item.js
+++ models/Item.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// Define collection and schema for AdUnits
+// Define collection and schema for Items
 let Items = new Schema({
   unit_name: {
     type: String
