c9db0c03aa01561da9d433ae1b2996a243a185e2
milan <milandima@gmail.com>
2018-08-14 14:29:11 +0200

    item.js

--- models/Item.js
+++ models/Item.js
@@ -1,18 +1,18 @@
-// AdUnit.js
+// Item.js
 
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Define collection and schema for AdUnits
-let AdUnit = new Schema({
+let Items = new Schema({
   unit_name: {
     type: String
   },
   unit_price: {
     type: Number
   }
-},{
-    collection: 'adunits'
-});
+}, {
+    collection: 'items'
+  });
 
-module.exports = mongoose.model('AdUnit', AdUnit);
+module.exports = mongoose.model('Items', Items);
